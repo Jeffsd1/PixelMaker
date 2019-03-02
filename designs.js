aef2f94fcf72c253cea2120e18064137fe15a8e5
Jeffsd1 <35415440+Jeffsd1@users.noreply.github.com>
2019-03-01 18:03:37 -0800

    Update designs.js

--- designs.js
+++ designs.js
@@ -4,16 +4,18 @@ function changeColor() {
     this.style.background = color;
 }
 
-function makeGrid() {
+
+
+function makeGrid() {     //  create makeGrid. Clear and create table based on user inputs 
     const gridHeight = document.getElementById("input_height").value;
     const gridWidth = document.getElementById("input_width").value;
     const pixelCanvas = document.getElementById("pixel_canvas"); 
     pixelCanvas.innerText=""; // empty table   
     
     for (let h=0; h<gridHeight; ++h) {
-        const row = pixelCanvas.insertRow(-1); // insert new row at the last position
+        const row = pixelCanvas.insertRow(-1); // insert new row for last position
         for (let w=0; w<gridWidth; ++w) {
-            const cell = row.insertCell(-1); //insert new cell at the last position
+            const cell = row.insertCell(-1); 
             cell.onclick = changeColor;
         }
     }
